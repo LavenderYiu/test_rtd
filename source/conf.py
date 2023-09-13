76b925cd92ab6675a2cb06cc941c74018ce48a76
lavenderyao <lavenderyao@tencent.com>
2023-09-13 21:06:21 +0800

    init

---
+++ source/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'sphinx_test'
+copyright = '2023, lavenderyao'
+author = 'lavenderyao'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    # "myst_parser",
+    "recommonmark"
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+breathe_projects = {"myproject": "F:/DCC/_Pipeline/doxygen/output/xml2"}
+breathe_default_project = "myproject"
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src'))
---
+++ source/index.rst
@@ -0,0 +1,31 @@
+.. sphinx_test documentation master file, created by
+   sphinx-quickstart on Mon Sep  4 11:28:46 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to sphinx_test's documentation!
+=======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+MarkDown Test MyST
+==================
+:doc:`test_myst`
+
+MarkDown Test recommonmark
+==========================
+ .. toctree::
+    :maxdepth: 2
+
+    test_recommonmark
